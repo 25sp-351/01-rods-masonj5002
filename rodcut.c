6276f6ab79e91b10d17a8c8b6051e7681e980450
Mason Jennings <masonj@csu.fullerton.edu>
2025-03-01 21:44:55 -0800

    created shortest length, updated recursive rodcut, fixed pointers

--- rodcut.c
+++ rodcut.c
@@ -4,71 +4,105 @@
 #define MAX_SIZE 200
 
 // TODO: ALLOW FUNCTION TO EXIT PROGRAM CLEANLY
-void userInput(int length[], int value[], int *rod_length, int *cut_list_size) {
-  int cut_length, cut_value = 0;
+void userInput(int *length, int *value, int *rod_length, int *cut_list_size) {
+    int cut_length, cut_value = 0;
 
-  while (scanf("%d,%d", &cut_length, &cut_value) == 2) { // 2 is not NULL
-    if (*cut_list_size >= MAX_SIZE) {
-      printf("MAXIMUM INPUT SIZE EXCEEDED - PROGRAM WILL EXIT.\n");
-      break;
+    while (scanf("%d,%d", &cut_length, &cut_value) == 2) {
+        if (*cut_list_size >= MAX_SIZE) {
+            printf("MAXIMUM INPUT SIZE EXCEEDED - PROGRAM WILL EXIT.\n");
+            break;
+        }
+
+        if (cut_length > *rod_length) {
+            printf("The cut is too long for this rod. Program will exit\n");
+            return;
+        }
+
+        length[*cut_list_size] = cut_length;
+        value[*cut_list_size]  = cut_value;
+        (*cut_list_size)++;
     }
+}
 
-    if (cut_length > *rod_length) {
-      printf("The cut is too long for this rod. Program will exit\n");
-      return;
-    }
-
-    length[*cut_list_size] = cut_length;
-    value[*cut_list_size] = cut_value;
-    (*cut_list_size)++;
-  }
+int shortestLength(int length[], int cut_list_size) {
+    int shortest = length[0];
+    for (int ix = 1; ix < cut_list_size; ix++)
+        if (shortest > length[ix])
+            shortest = length[ix];
+    return shortest;
 }
 
 int recursiveRodcut(int length[], int value[], int *rod_length,
                     int *cut_list_size) {
-  printf("HELLO FROM recursiveRodcut\n");
+    printf("HELLO FROM recursiveRodcut\n");
 
-  // ARE WE DONE? (BASE CASE)
-  // check if remainder is less than the smallest cut size
+    // BASE CASE
+    if (*rod_length < shortestLength(length, *cut_list_size)) {
+        printf("BASE CASE REACHED, returning: %d\n", 0);
+        return 0;
+    }
 
-  // LEAST WE CAN DO
-  // choose the cut that has the highest dollar amount
+    int highest_value_for_length       = 0;
+    int highest_value_for_length_index = -1;
 
-  // TRUST THAT SOMEONE ELSE CAN DO
-  // reduce the remainder and run it again
+    for (int iw = 1; iw < *cut_list_size; iw++) {
+        if (value[iw] > highest_value_for_length && length[iw] <= *rod_length) {
+            highest_value_for_length       = value[iw];
+            highest_value_for_length_index = iw;
+        }
+    }
 
-  return 0;
+    if (highest_value_for_length_index == -1) {
+        printf("NO VALID CUT FOUND\n");
+        return 0;
+    }
+
+    *rod_length -= (length[highest_value_for_length_index]);
+
+    // Recursive call
+    int recursive_result =
+        recursiveRodcut(length, value, rod_length, cut_list_size);
+    return (value[highest_value_for_length_index] + recursive_result);
 }
 
-// system output
-
 int main(int argc, char **argv) {
-  // Rod length exists
-  if (argc != 2) {
-    printf("USAGE: rod_length<int>\n");
-    return 1;
-  }
+    // Rod length exists
+    if (argc != 2) {
+        printf("USAGE: rod_length<int>\n");
+        return 1;
+    }
 
-  // convert rod length into int
-  int rod_length = atoi(argv[1]);
-  if (rod_length < 1) {
-    printf("INVALID ROD LENGTH.");
-    return 1;
-  }
-  printf("Rod length: %d\n", rod_length);
+    // convert rod length into int
+    int rod_length = atoi(argv[1]);
+    if (rod_length < 1) {
+        printf("INVALID ROD LENGTH.");
+        return 1;
+    }
+    printf("Rod length: %d\n", rod_length);
 
-  int cut_list_size = 0;
-  int length[MAX_SIZE];
-  int value[MAX_SIZE];
-  userInput(length, value, &rod_length, &cut_list_size);
+    int cut_list_size = 0;
+    int *length       = (int *)malloc(MAX_SIZE * sizeof(int));
+    int *value        = (int *)malloc(MAX_SIZE * sizeof(int));
 
-  // DEBUGGING OUTPUT
-  printf("\nYOU ENTERED:\n");
-  for (int i = 0; i < cut_list_size; i++) {
-    if (length[i] == 0 && value[i] == 0)
-      break;
-    printf("Length: %d, Value: %d\n", length[i], value[i]);
-  }
+    if (length == NULL || value == NULL) {
+        printf("MEMORY ALLOCATION FAILED.");
+        return 1;
+    }
 
-  return 0;
+    userInput(length, value, &rod_length, &cut_list_size);
+
+    // DEBUGGING OUTPUT
+    printf("\nYOU ENTERED:\n");
+    for (int i = 0; i < cut_list_size; i++) {
+        if (length[i] == 0 && value[i] == 0)
+            break;
+        printf("Length: %d, Value: %d\n", length[i], value[i]);
+    }
+
+    printf("\nSTARTING RECURSIVE RODCUT...\n");
+    printf("Profit: %d\n", recursiveRodcut(length, value, &rod_length, &cut_list_size));
+
+    free(length);
+    free(value);
+    return 0;
 }
